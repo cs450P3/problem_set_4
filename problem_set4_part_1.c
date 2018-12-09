982086c2fc7df9ef729049c625fa9f88a5df8055
David Tauraso <david318@me.com>
2018-12-08 19:26:01 -0800

    Did virtual address to physical address conversion

--- problem_set4_part_1.c
+++ problem_set4_part_1.c
@@ -23,7 +23,6 @@ int main()
 	// the virtual segment size = bits_specifying_virtual_page
 	// the remaining bits are the offset
 
-	int physical_page_number_offset = bits_in_physical_address - bits_specifying_virtual_page;
 
 	int size_of_segment = sizeof(char) * bits_specifying_virtual_page;
 
@@ -31,15 +30,18 @@ int main()
 
 	memcpy(virtual_segment, buffer, size_of_segment);
 	virtual_segment[size_of_segment] = '\0';
+
 	printf("%s\n", virtual_segment);
 
+	int physical_page_number_offset = bits_in_physical_address - bits_specifying_virtual_page;
 
 	int size_of_physical_offset = sizeof(char) * physical_page_number_offset;
 
 	char* physical_offset = malloc(size_of_physical_offset);
-	memcpy(physical_offset, buffer + size_of_segment, size_of_physical_offset);
 
+	memcpy(physical_offset, buffer + size_of_segment, size_of_physical_offset);
 	physical_offset[physical_page_number_offset] = '\0';
+
 	printf("%s\n", physical_offset);
 
 	int index = atoi(virtual_segment);
