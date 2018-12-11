77e2ba4ea6a4dc89ab27bb7fbeec715cb0b5e5d7
David Tauraso <david318@me.com>
2018-12-10 18:30:22 -0800

    Did testing

---
+++ complete_Prob1+2.cpp
@@ -0,0 +1,267 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <bitset>
+#include <math.h>
+#define DEBUG 0
+#define debug if (DEBUG) cout
+
+using namespace std;
+
+
+struct VirtualPage
+{
+    int valid;
+    int permission;
+    int ppn;
+    int useBit;
+};
+void printVirtualPage(VirtualPage page)
+{
+	printf("valid | permission | ppn | useBit\n");
+	printf("|    %i   |   %i   |    %i    |   %i   |\n",
+			page.valid, page.permission, page.ppn, page.useBit);
+}
+struct VirtualPageInfo
+{
+    int numBitsVirtAdd;
+    int numBitsPhysAdd;
+    int numByesInPage;
+};
+
+struct VirtToPhys
+{
+    /* useBit | virtualAddress | -> | physicalAddress */
+    int useBit;
+    int virtualAddress;
+    int physicalAddress;
+};
+
+void PrintVector(vector<VirtualPage> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        debug << "------------------" << endl;
+        debug << "pos #: " << i << endl;
+        debug << "-------" << endl;
+        debug << "valid: " << v[i].valid << endl;
+        debug << "permission: " << v[i].permission << endl;
+        debug << "ppn: " << v[i].ppn << endl;
+        debug << "useBit: " << v[i].useBit << endl;
+        debug << "------------------" << endl;
+    }
+}
+
+
+unsigned long long CalculatePhysicalAddress(int ppn, int moveIndex, int hexVal, const VirtualPageInfo &virtualPageInfo)
+{
+    /* Calculates the index for looking up in the virtual page table */
+    // int bitIndex = virtualPageInfo.numBitsPhysAdd - virtualPageInfo.numBitsVirtAdd + 1; // 2
+    // int shift = virtualPageInfo.numBitsVirtAdd - bitIndex; // 5
+
+    unsigned long long tagBits = virtualPageInfo.numBitsVirtAdd - moveIndex; // 3
+    unsigned long long insertIntoPhysicalAddress = (ppn << moveIndex);
+    /* insertIntoPhysicalAddress = 192 = 11000000
+     * all we care about are the first 3 bits = 110
+     * 8 bits */
+
+    /* Zero's out bits
+     * zero's out the first 2
+     * bits in the entered hex
+     * value. */
+    for (int n = moveIndex + 1; n < virtualPageInfo.numBitsVirtAdd; n++)
+    {
+        hexVal &= ~(1 << n);
+    }
+    /* hexVal = 10 = 0001010
+     * these first three bits
+     * will be masked with the
+     * first 3 bits of insertIntoPhysicalAddress */
+
+    unsigned long long physicalAddress = (hexVal | insertIntoPhysicalAddress);
+    /* physicalAddress = 202 = 11001010 = 0xCA
+     * 8 bits */
+    return physicalAddress;
+}
+
+int main(int argc, char ** argv)
+{
+    if (argc < 2)
+    {
+        cout << "Forgot file name" << endl;
+        exit(1);
+    }
+
+    /* Reading in values from file */
+    int temp = 0;
+
+    vector <VirtualPage> virtualPages;
+    ifstream file;
+    file.open(argv[1]);
+
+    /* Get virtual page information */
+    VirtualPageInfo virtualPageInfo;
+    file >> temp;
+    virtualPageInfo.numBitsVirtAdd = temp;
+    file >> temp;
+    virtualPageInfo.numBitsPhysAdd = temp;
+    file >> temp;
+    virtualPageInfo.numByesInPage = temp;
+
+    /* Get virtual page information */
+    while (file >> temp)
+    {
+        VirtualPage virtualPage;
+        virtualPage.valid = temp;
+        file >> temp;
+        virtualPage.permission = temp;
+        file >> temp;
+        virtualPage.ppn = temp;
+        file >> temp;
+        virtualPage.useBit = temp;
+
+        virtualPages.push_back(virtualPage);
+    }
+    file.close();
+
+    /* The clock algo executes on this vector */
+    vector<VirtToPhys> virtToPhysMap;
+
+    for (int i = 0; i < virtualPages.size(); i++)
+    {
+        if (virtualPages[i].valid && virtualPages[i].permission)
+        {
+            VirtToPhys vtp;
+            vtp.useBit = virtualPages[i].useBit;
+            vtp.physicalAddress = virtualPages[i].ppn;
+            vtp.virtualAddress = i;
+
+            virtToPhysMap.push_back(vtp);
+        }
+    }
+
+    int moveIndex = log2(virtualPageInfo.numByesInPage);
+    double moveIndexCheck = log2(virtualPageInfo.numByesInPage);
+
+    /* Checking to make sure log2 returned an integer value */
+    if (moveIndex != moveIndexCheck)
+    {
+        cout << "incorrect BYTES in a page" << endl;
+        exit(1);
+    }
+
+    /* CLOCK ALGO position in
+     * the VirtToPhysMap 'table' */
+    int clockAlgo = 0;
+
+    while (true)
+    {
+        unsigned long long hexVal = 0;
+        cout << ">>> ";
+        cin >> hex >> hexVal;
+
+        /* Gets the virtual index */
+        unsigned long long indexVirt = (hexVal >> moveIndex);
+        bool segfault = false;
+        bool disk = false;
+
+        if (virtualPages[indexVirt].valid == 0 && virtualPages[indexVirt].permission == 1)
+        {
+            #ifdef PROB1
+            disk = true;
+            #else
+            disk = true;
+
+            /* CLOCK ALGO */
+
+            VirtToPhys * virtToPhys;
+            for (; ; clockAlgo++)
+            {
+                if (clockAlgo > virtToPhysMap.size() - 1)
+                {
+                    debug << "setting clockAlgo to 0" << endl;
+                    clockAlgo = 0;
+                }
+
+                virtToPhys = &virtToPhysMap[clockAlgo];
+
+                if (virtToPhysMap[clockAlgo].useBit == 0)
+                {
+                    debug << "executing clock algo on idx: " << clockAlgo << endl;
+
+                    /* set original virtual address bits to: */
+                    virtualPages[virtToPhys->virtualAddress].valid = 0;
+                    virtualPages[virtToPhys->virtualAddress].useBit = 0;
+                    virtualPages[virtToPhys->virtualAddress].ppn = -1;
+
+                    /* Mark the page as used */
+                    virtToPhys->useBit = 1;
+
+                    /* Change virtual page number */
+                    virtToPhys->virtualAddress = indexVirt;
+                    /* Set virtual page table ppn */
+                    virtualPages[indexVirt].ppn = virtToPhys->physicalAddress;
+                    /* Validate the virtual page table entry */
+                    virtualPages[indexVirt].valid = 1;
+                    /* Just in case permissions was previously set to 0,
+                    * now we have permission to access ppn. */
+                    virtualPages[indexVirt].permission = 1;
+                    virtualPages[indexVirt].useBit = 1;
+
+                    break;
+                }
+                else
+                {
+                    virtToPhysMap[clockAlgo].useBit = 0;
+                    virtualPages[virtToPhys->virtualAddress].useBit = 0;
+                }
+
+            }
+
+            /* After the clock algo finishes, we must advance its position.
+            * If the clock algo is at the last position move its position
+            * to the first element. */
+            if (clockAlgo == virtToPhysMap.size() - 1)
+                clockAlgo = 0;
+            else
+                clockAlgo++;
+
+            debug << "clock algo position (right after for loop): " << clockAlgo << endl;
+
+            #endif
+        }
+        else if (virtualPages[indexVirt].permission == 0)
+            segfault = true;
+		//printVirtualPage(virtualPages[indexVirt]);
+
+        if (segfault)
+            cout << "Physical Address: SEGFAULT" << endl;
+    #ifdef PROB1
+        else if (disk)
+            cout << "Physical Address: DISK" << endl;
+    #else
+        else if (disk)
+        {
+            printf("Physical Address (PAGE FAULT): 0x%llX\n", CalculatePhysicalAddress(virtualPages[indexVirt].ppn, moveIndex, hexVal, virtualPageInfo));
+        }
+    #endif
+        else
+        {
+            printf("Physical Address: 0x%llX\n", CalculatePhysicalAddress(virtualPages[indexVirt].ppn, moveIndex, hexVal, virtualPageInfo));
+            virtualPages[indexVirt].useBit = 1;
+            for (int i = 0; i < virtToPhysMap.size(); i++)
+            {
+                if (virtToPhysMap[i].virtualAddress == indexVirt)
+                    virtToPhysMap[i].useBit = 1;
+            }
+
+        }
+
+        debug << endl << "Debugging Info: " << endl;
+        PrintVector(virtualPages);
+        debug << "clock algo position: " << clockAlgo << endl;
+        debug << "indexVirt: " << indexVirt << endl;
+    }
+
+    return 0;
+}
--- p1.cpp
+++ p1.cpp
@@ -6,58 +6,243 @@
 #include <stdlib.h>
 #include <typeinfo>
 #include <vector>
-
+#include <deque>
+#include <cmath>
+// this program only converts virtual address to physical addresses
 using namespace std;
+template <class T>
+void printBitVector(T bit_virtual_address)
+{
+	for(int i = 0; i < bit_virtual_address.size(); i++)
+	{
+		if(i % 4 == 0)
+		{
+			printf("   ");
+		}
+		printf("%i ", bit_virtual_address[i]);
 
-int main() {
-    ifstream inFile;
-    int num = 0;
-    int virtual_address;
-    int physical_address;
-    int bytes;
+	}
+}
+deque<int> valueToBitVector(int the_virtual_address,
+							 deque<int> bit_virtual_address)
+{
+	printf("the virtual address is %x\n", the_virtual_address);
+	//int tracker = 1;
+	while(the_virtual_address > 0)
+	{
 
-    inFile.open("test.txt");
-    if (!inFile) {
-        cout << "Unable to open file";
-        exit(1); // terminate with error
-    }
+		//if(tracker & the_virtual_address)
+		//{
+			//printf("%i ", 1);
+		bit_virtual_address.push_back(the_virtual_address % 2);
+		the_virtual_address /= 2;
+		//}
+		//else
+		//{
+		//	printf("%i ", 0);
+		//	bit_virtual_address.push_back(0);
 
-    if (inFile >> num){
+		//}
+
+		//tracker = tracker << 1;
+	}
+	// padding added to begining
+	int j = bit_virtual_address.size();
+	while(j % 4 != 0 )
+	{
+		//printf("%i ", 0);
+		bit_virtual_address.push_back(0);
+		j++;
+	}
+	//cout << endl;
+
+	reverse(bit_virtual_address.begin(), bit_virtual_address.end());
+	printBitVector(bit_virtual_address);
+	cout << endl;
+	return bit_virtual_address;
+}
+deque<int> getVirtualAddress(deque<int> bit_vector, int virtual_address_size)
+{
+	deque<int> virtual_address;
+	for(int i = 0; i < virtual_address_size; i++)
+	{
+		virtual_address.push_front(bit_vector[virtual_address_size-i]);
+	}
+	return virtual_address;
+}
+int bitVectorToValue(vector<int> bit_virtual_address)
+{
+	//cout << "in bit to vector\n";
+	reverse(bit_virtual_address.begin(), bit_virtual_address.end());
+	int value = 0;
+	for(int i = 0; i < bit_virtual_address.size(); i++)
+	{
+		//cout << value <<  bit_virtual_address[i] << endl;
+		value +=  bit_virtual_address[i] * (int) pow(2.0, i);
+	}
+	return value;
+}
+
+int countDigits(int number)
+{
+	int count = 0;
+	while(number > 0)
+	{
+		number /= 10;
+		count++;
+		//printf("%i ", number);
+	}
+	//printf("%i\n", count);
+	return count;
+}
+int main(int argc, char** argv) {
+
+
+	ifstream inFile;
+	int num = 0;
+	int virtual_address;
+	int physical_address;
+	int bytes_in_physical_page;
+
+	inFile.open("test.txt");
+	if (!inFile) {
+		cout << "Unable to open file";
+		exit(1); // terminate with error
+	}
+
+	if (inFile >> num){
 		virtual_address = num;
 		if (inFile >> num){
 			physical_address = num;
 			if(inFile >> num){
-				bytes = num;
+				bytes_in_physical_page = num;
 			}
 		}
-		
+
 	}
 
 	vector<int> row;
-    while(inFile >> num){
-    	row.push_back(num);
-    }
+	while(inFile >> num){
+		row.push_back(num);
+	}
 
-    vector<int> valid;
-    vector<int> permission;
-    vector<int> pgNum;
-    vector<int> useBit;
-    for(int i = 0; i < row.size(); i++){
-    	//printf("%i ", row[i]);
-    	if( i % 4 == 0){
-    		valid.push_back(row[i]);
-    	} 
-    	if(i % 4 == 1){
-    		permission.push_back(row[i]);
-    	}
-    	if(i % 4 == 2){
-    		pgNum.push_back(row[i]);
-    	}
-    	if (i % 4 == 3){
-    		useBit.push_back(row[i]);
-    	}
-    }
-   
-    inFile.close();
+	vector<int> valid;
+	vector<int> permission;
+	vector<int> pgNum;
+	vector<int> useBit;
+	for(int i = 0; i < row.size(); i++){
+		//printf("%i ", row[i]);
+		if( i % 4 == 0){
+			valid.push_back(row[i]);
+		}
+		if(i % 4 == 1){
+			permission.push_back(row[i]);
+		}
+		if(i % 4 == 2){
+			pgNum.push_back(row[i]);
+		}
+		if (i % 4 == 3){
+			useBit.push_back(row[i]);
+		}
+	}
+
+	// todo: put in sanity checks
+
+	int the_virtual_address = 0x28;
+	// from value to bit vector
+	deque<int> bit_virtual_address;
+	bit_virtual_address = getVirtualAddress(valueToBitVector(the_virtual_address, bit_virtual_address), virtual_address);
+
+	printBitVector(bit_virtual_address);
+
+
+	while(bit_virtual_address.size() > virtual_address)
+	{
+		bit_virtual_address.pop_front();
+	}
+	printf("\n");
+	//printBitVector(bit_virtual_address);
+	//exit(1);
+	//printf("\nanswer %x\n", value);
+	printf("\n");
+	//char buffer[50];
+	//sprintf(buffer, "%x", the_virtual_address);
+	//printf("%s\n", buffer);
+	/*
+	log2() Parameters
+	The log2() function takes a single mandatory argument in the range [0, ∞].
+	If the value is less than zero, log2() returns NaN (Not a Number).
+	*/
+
+
+	int offset_number_of_bits_representing_physical_page_size = (int) log2(bytes_in_physical_page);
+	printf("offset to physical page size %i\n", offset_number_of_bits_representing_physical_page_size);
+
+	int segment_size = virtual_address - offset_number_of_bits_representing_physical_page_size;
+	printf("segment index size %i\n", segment_size);
+
+
+	vector<int> segment;
+	for(int i = 0; i < segment_size; i++)
+	{
+		segment.push_back(bit_virtual_address[i]);
+	}
+	for(int i = 0; i < segment.size(); i++)
+	{
+		cout << segment[i];
+
+	}
+
+	cout << endl;
+	int index = bitVectorToValue(segment);
+	cout << "the index to the table is " << index << endl;
+
+
+	vector<int> physical_page_offset;
+	for(int i = segment_size; i < segment_size + offset_number_of_bits_representing_physical_page_size; i++)
+	{
+		physical_page_offset.push_back(bit_virtual_address[i]);
+	}
+
+	printf("the physical page offset is ");
+	printBitVector(physical_page_offset);
+	printf("\n");
+	if(index >= pgNum.size())
+	{
+		cout << "page " << index << " does not exist\n";
+		cout << pgNum.size() << endl;
+	}
+	else
+	{
+		cout << "accessing page table\n";
+		//cout << pgNum[index] << endl;
+		deque<int> physical_page_number;
+
+		physical_page_number = valueToBitVector(pgNum[index], physical_page_number);
+		//printBitVector(physical_page_number);
+		//printBitVector(physical_page_offset);
+
+
+		vector<int> physical_address_;
+
+		for(int i = 0; i < physical_page_number.size(); i++)
+		{
+			physical_address_.push_back(physical_page_number[i]);
+		}
+		for(int i = 0; i < physical_page_offset.size(); i++)
+		{
+			physical_address_.push_back(physical_page_offset[i]);
+		}
+		printf("\nphysical address to mem\n");
+		printBitVector(physical_address_);
+
+		int physical_address_to_mem = bitVectorToValue(physical_address_);
+		printf("\nfinal address is %x", physical_address_to_mem);
+
+	}
+	cout << endl;
+	inFile.close();
+
+
     return 0;
 }
